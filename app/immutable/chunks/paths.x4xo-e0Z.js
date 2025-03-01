d3ac2dd323390946e22ccf15fa5eb2581c6e37dd
dianayukari <yukaridiana@gmail.com>
2025-03-01 21:23:01 +0100

    Updates

---
+++ app/immutable/chunks/paths.x4xo-e0Z.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ajnt17)==null?void 0:s.base)??"/museum";var a;const e=((a=globalThis.__sveltekit_ajnt17)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ app/immutable/entry/start.DWo04NDp.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D9Yuy8JA.js";export{t as start};
